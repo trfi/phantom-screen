cf7bc7f84338c0f55a849a51aa5ebc625316fe4a
BypassCore Labs <labs@bypasscore.com>
2022-05-27 20:11:38 +0000

    Add inline hooking support\n\nImplement inline/detour hooking with trampoline generation. Includes\na simplified x86-64 length disassembler for calculating minimum\npatch sizes. Supports both 32-bit relative and 64-bit absolute jumps.

--- src/core/hooks.cpp
+++ src/core/hooks.cpp
@@ -198,23 +198,218 @@ size_t HookEngine::active_hook_count() const {
 }
 
 // Stub implementations for inline/VMT (will be filled in later commits)
+// ---- Inline Length Disassembler (simplified) ----
+// Returns instruction length at the given address.
+// This is a simplified x86-64 length disassembler covering common
+// instruction prefixes and encodings. A production implementation
+// would use a full disassembler like Zydis or Capstone.
+static size_t get_instruction_length(const uint8_t* code) {
+    size_t len = 0;
+    bool has_rex = false;
+    bool has_prefix66 = false;
+
+    // Skip prefixes
+    while (true) {
+        uint8_t b = code[len];
+        if (b == 0x66) { has_prefix66 = true; len++; continue; }
+        if (b == 0x67 || b == 0xF2 || b == 0xF3 ||
+            b == 0x2E || b == 0x36 || b == 0x3E || b == 0x26 ||
+            b == 0x64 || b == 0x65) { len++; continue; }
+        // REX prefix (0x40-0x4F)
+        if ((b & 0xF0) == 0x40) { has_rex = true; len++; continue; }
+        break;
+    }
+
+    uint8_t opcode = code[len++];
+
+    // Common single-byte instructions
+    if (opcode == 0x90) return len; // NOP
+    if (opcode == 0xC3 || opcode == 0xCB) return len; // RET
+    if (opcode == 0xCC) return len; // INT3
+
+    // PUSH/POP register (0x50-0x5F)
+    if (opcode >= 0x50 && opcode <= 0x5F) return len;
+
+    // MOV reg, imm32/imm64 (0xB8-0xBF)
+    if (opcode >= 0xB8 && opcode <= 0xBF) {
+        return len + (has_rex ? 8 : 4);
+    }
+
+    // Short JMP rel8
+    if (opcode == 0xEB) return len + 1;
+
+    // Near JMP rel32
+    if (opcode == 0xE9) return len + 4;
+
+    // CALL rel32
+    if (opcode == 0xE8) return len + 4;
+
+    // Jcc rel8 (0x70-0x7F)
+    if (opcode >= 0x70 && opcode <= 0x7F) return len + 1;
+
+    // SUB RSP, imm8 pattern
+    if (opcode == 0x83) {
+        len++; // ModR/M
+        return len + 1; // imm8
+    }
+
+    // MOV with ModR/M
+    if (opcode == 0x89 || opcode == 0x8B || opcode == 0x8D) {
+        uint8_t modrm = code[len++];
+        uint8_t mod = (modrm >> 6) & 3;
+        uint8_t rm = modrm & 7;
+        if (mod == 0 && rm == 5) return len + 4; // RIP-relative
+        if (mod == 0 && rm == 4) len++; // SIB
+        if (mod == 1) { if (rm == 4) len++; return len + 1; }
+        if (mod == 2) { if (rm == 4) len++; return len + 4; }
+        return len;
+    }
+
+    // Two-byte opcode (0x0F prefix)
+    if (opcode == 0x0F) {
+        uint8_t op2 = code[len++];
+        // Jcc rel32 (0x80-0x8F)
+        if (op2 >= 0x80 && op2 <= 0x8F) return len + 4;
+        // MOVAPS, MOVDQA etc with ModR/M
+        uint8_t modrm = code[len++];
+        uint8_t mod = (modrm >> 6) & 3;
+        uint8_t rm = modrm & 7;
+        if (mod == 0 && rm == 5) return len + 4;
+        if (mod == 0 && rm == 4) len++;
+        if (mod == 1) { if (rm == 4) len++; return len + 1; }
+        if (mod == 2) { if (rm == 4) len++; return len + 4; }
+        return len;
+    }
+
+    // Fallback: assume 1 byte
+    return len;
+}
+
 size_t HookEngine::calculate_trampoline_size(void* target) {
-    // Basic implementation: minimum 5 bytes for a JMP rel32
-    // A proper implementation needs a length disassembler
-    (void)target;
-    return 5;
+    const uint8_t* code = static_cast<const uint8_t*>(target);
+    size_t total = 0;
+    // Need at least 5 bytes for a JMP rel32 (or 14 for absolute JMP on x64)
+#ifdef _WIN64
+    const size_t min_size = 14; // JMP [RIP+0]; DQ addr
+#else
+    const size_t min_size = 5;  // JMP rel32
+#endif
+    while (total < min_size) {
+        size_t inst_len = get_instruction_length(code + total);
+        if (inst_len == 0) return 0; // Disassembly failure
+        total += inst_len;
+    }
+    return total;
 }
 
 void* HookEngine::create_trampoline(void* target, size_t patch_size) {
-    (void)target;
-    (void)patch_size;
-    return nullptr; // Implemented in inline hook commit
+    // Allocate trampoline near target for relative addressing
+    size_t tramp_size = patch_size + 14; // copied bytes + JMP back
+    void* trampoline = Memory::alloc_near(target, tramp_size);
+    if (!trampoline) return nullptr;
+
+    // Copy original bytes
+    memcpy(trampoline, target, patch_size);
+
+    // Add JMP back to original code after the patched region
+    uint8_t* jmp_back = static_cast<uint8_t*>(trampoline) + patch_size;
+    void* continue_addr = static_cast<uint8_t*>(target) + patch_size;
+
+#ifdef _WIN64
+    // Absolute JMP: FF 25 00 00 00 00 [8-byte addr]
+    jmp_back[0] = 0xFF;
+    jmp_back[1] = 0x25;
+    *reinterpret_cast<uint32_t*>(jmp_back + 2) = 0;
+    *reinterpret_cast<uint64_t*>(jmp_back + 6) = reinterpret_cast<uint64_t>(continue_addr);
+#else
+    // Relative JMP: E9 [4-byte offset]
+    jmp_back[0] = 0xE9;
+    *reinterpret_cast<int32_t*>(jmp_back + 1) = Memory::calculate_relative(jmp_back, continue_addr);
+#endif
+
+    Memory::flush_cache(trampoline, tramp_size);
+    return trampoline;
 }
 
 Status HookEngine::install_inline_hook(const std::string& name, void* target,
                                         void* detour, void** original_out) {
-    (void)name; (void)target; (void)detour; (void)original_out;
-    return Status::ErrorNotSupported; // Implemented later
+    std::lock_guard<std::mutex> lock(mutex_);
+
+    if (hooks_.find(name) != hooks_.end()) {
+        return Status::ErrorAlreadyInitialized;
+    }
+
+    if (!target || !detour) return Status::ErrorInvalidParam;
+
+    // Calculate how many bytes we need to overwrite
+    size_t patch_size = calculate_trampoline_size(target);
+    if (patch_size == 0) {
+        PHANTOM_ERROR("Failed to calculate trampoline size for " + name);
+        return Status::ErrorHookFailed;
+    }
+
+    // Create trampoline (copy original bytes + JMP back)
+    void* trampoline = create_trampoline(target, patch_size);
+    if (!trampoline) {
+        PHANTOM_ERROR("Failed to create trampoline for " + name);
+        return Status::ErrorMemoryAlloc;
+    }
+
+    if (original_out) *original_out = trampoline;
+
+    // Backup original bytes
+    void* backup = Memory::alloc_near(target, patch_size);
+    if (backup) {
+        memcpy(backup, target, patch_size);
+    }
+
+    // Write the detour JMP at the target
+    uint8_t jmp_patch[14] = {};
+#ifdef _WIN64
+    // FF 25 00 00 00 00 [8-byte addr]
+    jmp_patch[0] = 0xFF;
+    jmp_patch[1] = 0x25;
+    *reinterpret_cast<uint32_t*>(jmp_patch + 2) = 0;
+    *reinterpret_cast<uint64_t*>(jmp_patch + 6) = reinterpret_cast<uint64_t>(detour);
+
+    // NOP remaining bytes
+    if (!Memory::safe_write(target, jmp_patch, 14)) {
+        Memory::free(trampoline);
+        Memory::free(backup);
+        return Status::ErrorHookFailed;
+    }
+    if (patch_size > 14) {
+        Memory::nop_fill(static_cast<uint8_t*>(target) + 14, patch_size - 14);
+    }
+#else
+    jmp_patch[0] = 0xE9;
+    *reinterpret_cast<int32_t*>(jmp_patch + 1) = Memory::calculate_relative(target, detour);
+
+    if (!Memory::safe_write(target, jmp_patch, 5)) {
+        Memory::free(trampoline);
+        Memory::free(backup);
+        return Status::ErrorHookFailed;
+    }
+    if (patch_size > 5) {
+        Memory::nop_fill(static_cast<uint8_t*>(target) + 5, patch_size - 5);
+    }
+#endif
+
+    HookContext ctx{};
+    ctx.name = name;
+    ctx.type = HookType::Inline;
+    ctx.target = target;
+    ctx.detour = detour;
+    ctx.trampoline = trampoline;
+    ctx.original_bytes = backup;
+    ctx.patch_size = patch_size;
+    ctx.active = true;
+
+    hooks_[name] = ctx;
+
+    PHANTOM_INFO("Inline hook installed: " + name + " (patched " +
+                 std::to_string(patch_size) + " bytes)");
+    return Status::Success;
 }
 
 Status HookEngine::install_vmt_hook(const std::string& name, void** vtable,
