9a98b3c9b67a19c0e8e797e60bad47639f26ccf9
trfi <truongbuidp@gmail.com>
2026-03-29 15:01:08 +0700

    Enhance overlay functionality: Add <dxgi1_2.h> include for improved DirectX integration.

--- src/display/overlay.cpp
+++ src/display/overlay.cpp
@@ -1,6 +1,7 @@
 #include <phantom/display/overlay.h>
 #include <phantom/display/cloaker.h>
 #include <phantom/utils/logger.h>
+#include <dxgi1_2.h>
 
 #pragma comment(lib, "d3d11.lib")
 #pragma comment(lib, "dxgi.lib")
